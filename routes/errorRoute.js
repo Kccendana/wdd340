5c59c8b89f43f8fcdb4fa9e8a413f43fae60ea57
kcccendana <kc_telyn@yahoo.com>
2025-05-24 03:41:20 +0900

    add routes

---
+++ routes/errorRoute.js
@@ -0,0 +1,12 @@
+// routes/errorRoute.js
+const express = require("express");
+const router = express.Router();
+const errorController = require("../controllers/errorController");
+const utilities = require("../utilities");
+
+router.get(
+  "/trigger-error",
+  utilities.handleErrors(errorController.errorTest)
+);
+
+module.exports = router;
